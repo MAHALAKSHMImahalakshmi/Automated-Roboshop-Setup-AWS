e3eef940d71828767fa799d7eef9c400a512d8bf
MAHALAKSHMImahalakshmi <athamahalakshmi27@gmail.com>
2025-05-25 11:38:53 +0530

    instance and update r53

--- roboshop.sh
+++ roboshop.sh
@@ -13,12 +13,12 @@ do
     if [ $instance != "frontend" ]
     then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
-        RECORD_NAME="${instance}.${DOMAIN_NAME}"
+        RECORD_NAME="${instance}.srivenkata.shop"
 
 
     else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
-        RECORD_NAME="$DOMAIN_NAME"
+        RECORD_NAME="srivenkata.shop"
     fi
     echo "$instance IP address: $IP"
     echo "Record Name: $RECORD_NAME"
