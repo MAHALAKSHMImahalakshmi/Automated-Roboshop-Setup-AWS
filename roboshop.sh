44c2ffd684fe319fc0813966af27e02ed3995541
MAHALAKSHMImahalakshmi <athamahalakshmi27@gmail.com>
2025-05-25 17:43:08 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -4,21 +4,21 @@ AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-0499ce052d4a6c9a2"
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
 ZONE_ID="Z041097235OVJ7AUAD5IE"
-DOMAINE_NAME="srivenkata.shop"
+DOMAIN_NAME="srivenkata.shop"
 
-for instance in ${INSTANCES[@]}
+for instance in $@
 
 do
     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-0499ce052d4a6c9a2 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
     then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
-        RECORD_NAME="${instance}.srivenkata.shop"
+        RECORD_NAME="$instance.$DOMAIN_NAME"
 
 
     else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
-        RECORD_NAME="srivenkata.shop"
+        RECORD_NAME="$DOMAIN_NAME"
     fi
     echo "$instance IP address: $IP"
     echo "Record Name: $RECORD_NAME"
