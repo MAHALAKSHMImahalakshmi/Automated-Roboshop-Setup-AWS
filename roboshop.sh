7466dc1d3e96bf8a87db08e462205b27ebff8ca8
MAHALAKSHMImahalakshmi <athamahalakshmi27@gmail.com>
2025-05-25 11:22:57 +0530

    instance and update r53

--- roboshop.sh
+++ roboshop.sh
@@ -6,16 +6,10 @@ INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipp
 ZONE_ID="Z041097235OVJ7AUAD5IE"
 DOMAINE_NAME="srivenkata.shop"
 
-for instance in "${INSTANCES[@]}"
+for instance in ${INSTANCES[@]}
+
 do
-    echo "Launching instance: $instance"
-    INSTANCE_ID=$(aws ec2 run-instances \
-        --image-id "$AMI_ID" \
-        --instance-type t2.micro \
-        --security-group-ids "$SG_ID" \
-        --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
-        --query "Instances[0].InstanceId" --output text)
-    
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-0499ce052d4a6c9a2 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
     then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
