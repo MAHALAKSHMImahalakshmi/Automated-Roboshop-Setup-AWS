abcf57b0eb40cf5f035fcd6196c8257eea987fdf
MAHALAKSHMImahalakshmi <athamahalakshmi27@gmail.com>
2025-05-25 11:17:32 +0530

    instance and update r53

--- roboshop.sh
+++ roboshop.sh
@@ -15,14 +15,7 @@ do
         --security-group-ids "$SG_ID" \
         --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
         --query "Instances[0].InstanceId" --output text)
-    if [ -z "$INSTANCE_ID" ]; then
-        echo "Failed to launch instance for $instance"
-        exit 1
-    fi
-    echo "Instance ID: $INSTANCE_ID"
-
-    aws ec2 wait instance-running --instance-ids $INSTANCE_ID
-
+    
     if [ $instance != "frontend" ]
     then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
@@ -31,32 +24,23 @@ do
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
         RECORD_NAME="$DOMAIN_NAME"
     fi
-
-    if [ -z "$IP" ]; then
-        echo "Failed to retrieve IP address for $instance"
-        exit 1
-    fi
     echo "$instance IP address: $IP"
 
-    echo "Updating Route 53 for $RECORD_NAME with IP $IP"
     aws route53 change-resource-record-sets \
     --hosted-zone-id $ZONE_ID \
-    --change-batch '{
-        "Comment": "Creating or Updating a record set for cognito endpoint",
-        "Changes": [{
-            "Action"              : "UPSERT",
-            "ResourceRecordSet"  : {
-                "Name"              : "'$RECORD_NAME'",
-                "Type"             : "A",
-                "TTL"              : 300,
-                "ResourceRecords"  : [{
-                    "Value"         : "'$IP'"
-                }]
-            }
+    --change-batch '
+    {
+        "Comment": "Creating or Updating a record set for cognito endpoint"
+        ,"Changes": [{
+        "Action"              : "UPSERT"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$RECORD_NAME'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$IP'"
+            }]
+        }
         }]
     }'
-    if [ $? -ne 0 ]; then
-        echo "Failed to update Route 53 for $instance"
-        exit 1
-    fi
 done
