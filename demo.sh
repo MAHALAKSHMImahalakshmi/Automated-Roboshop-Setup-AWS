907cb6b73462fc8d2b4637092b5363e3938de9d8
MAHALAKSHMImahalakshmi <athamahalakshmi27@gmail.com>
2025-07-14 22:08:36 +0530

    demo

--- demo.sh
+++ demo.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
-dnf module disable nodejs -y
+VALIDATE(){
 
+    if [  $1 -eq 0 ]
+    then    
+        echo -e "$2 is ...... $G SUCCESS $N " | tee -a $LOG_FILE
+    else
+        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    fi  
+}
+
+dnf module disable nodejs -y
+VALIDATE $? "disable "
 
 dnf module enable nodejs:20 -y
+VALIDATE $? "enable"
 
 dnf install nodejs -y
+VALIDATE $? "Install"
 
 cp catalogue.service /etc/systemd/system/catalogue.service
-
+VALIDATE $? "service cata copy"
